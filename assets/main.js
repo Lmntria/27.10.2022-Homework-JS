ec49543137097dd26177a0063e0e654de3ad1635
Lmntria <seymur1095@gmail.com>
2022-10-27 17:21:51 +0400

    finish

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+//First version
+// const firstArr=[1,3,5,9,7]
+// const secondArr=[1,3,5,9,7]
+// const thirdArr=[1,3,5,9,8]
+
+
+// function isEqual(a,b) {
+//   if (JSON.stringify(a)==JSON.stringify(b)) {
+//     return true
+//   }
+//   else{
+//     return false
+//   }
+// }
+
+// console.log(isEqual(firstArr,secondArr))
+// console.log(isEqual(firstArr,thirdArr))
